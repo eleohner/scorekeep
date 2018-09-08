231e8a5f82be958c8a5773a84f8955e52839cfe1
Eric Leohner <eleohner@gmail.com>
2018-09-08 08:25:41 -0400

    Added flex styles to player boxes

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -7,35 +7,42 @@ export default class Player extends React.Component {
   render() {
     return (
       <div className="item" key={this.props.player._id}>
-        <p>
-          {this.props.player.name} has {this.props.player.score}{" "}
-          {this.props.player.score == 1 || this.props.player.score == -1
-            ? "point"
-            : "points"}
-          .
-        </p>
-        <button
-          className="button button--round"
-          onClick={_ =>
-            Players.update(this.props.player._id, { $inc: { score: -1 } })
-          }
-        >
-          -1
-        </button>
-        <button
-          className="button button--round"
-          onClick={_ =>
-            Players.update(this.props.player._id, { $inc: { score: 1 } })
-          }
-        >
-          +1
-        </button>
-        <button
-          className="button button--round"
-          onClick={_ => Players.remove({ _id: this.props.player._id })}
-        >
-          X
-        </button>
+        <div className="player">
+          <div>
+            <h3 className="player__name">{this.props.player.name}</h3>
+            <p className="player__stats">
+              {this.props.player.score + " "}
+              {this.props.player.score == 1 || this.props.player.score == -1
+                ? "point"
+                : "points"}
+              .
+            </p>
+          </div>
+          <div className="player__actions">
+            <button
+              className="button button--round"
+              onClick={_ =>
+                Players.update(this.props.player._id, { $inc: { score: -1 } })
+              }
+            >
+              -1
+            </button>
+            <button
+              className="button button--round"
+              onClick={_ =>
+                Players.update(this.props.player._id, { $inc: { score: 1 } })
+              }
+            >
+              +1
+            </button>
+            <button
+              className="button button--round"
+              onClick={_ => Players.remove({ _id: this.props.player._id })}
+            >
+              X
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
